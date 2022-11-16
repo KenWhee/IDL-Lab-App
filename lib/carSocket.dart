ef774bdf433476b6544af26d7d67692c4562b946
Kenneth-del <61149956+Kenneth-del@users.noreply.github.com>
2022-11-16 11:50:08 -0500

    comment

--- lib/carSocket.dart
+++ lib/carSocket.dart
@@ -1,2 +1,22 @@
 import 'client.dart';
 import 'main.dart';
+import 'car.dart';
+
+carSocket() {
+  // createSocket();
+
+  /*
+  Client: I want car state information
+  Client: I want what cars are connected to you
+  Server: Sends Information
+
+  */
+}
+// if we get a hostname we can assume it's online
+// if it desired_pos != pos_X assign instruction status
+//  then lock car card
+
+// update list 10 seconds
+
+
+
